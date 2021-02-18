cedb224a88ecdd4a96db4c8b4b7605f396e0d186
Fagner Carvalho <fagnerlc@outlook.com>
2021-02-18 17:25:06 -0300

    Tela de Detalhes e Componente de Time

--- lib/app/modules/new_task/new_task_page.dart
+++ lib/app/modules/new_task/new_task_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/app/modules/new_task/new_task_controller.dart';
+import 'package:todo_list/app/shared/time_component.dart';
 
 class NewTaskPage extends StatelessWidget {
   static String routerName = '/new';
@@ -10,11 +11,99 @@ class NewTaskPage extends StatelessWidget {
       builder: (BuildContext context, NewTaskController controller, _) {
         return Scaffold(
           appBar: AppBar(
-            title: Text('Nova Task'),
+            backgroundColor: Colors.white,
+            elevation: 0,
           ),
-          body: Container(
-            child: Center(
-              child: Text(''),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'NOVA TASK',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Data',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '16/02/2021',
+                    style: TextStyle(
+                        color: Colors.grey[800],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Nome da Task',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Hora',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TimeComponent(),
+                  SizedBox(height: 50),
+                  Center(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 40,
+                      //height: MediaQuery.of(context).size.height,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(0),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 30,
+                              color: Theme.of(context).primaryColor),
+                        ],
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      child: Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Text(
+                              'Salvar',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         );
